f3a3e784e5e428e565e4eb23075196a39b0ef07f
unknown <tuanbao97@gmail.com>
2026-08-14 19:28:27 +0700

    update

--- database/seeders/DeployAuthSeeder.php
+++ database/seeders/DeployAuthSeeder.php
@@ -4,20 +4,25 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+/**
+ * Seed tối thiểu để đăng nhập admin khi deploy.
+ *
+ * Chạy trên server:
+ *   php artisan db:seed --class=DeployAuthSeeder
+ *
+ * Tài khoản mặc định (AuthConstant):
+ *   Email/Username: dochoiwinwin@gmail.com
+ *   Password: Dochoiwinwin2026@?!
+ *
+ * Lưu ý Passport: nếu chưa có key thì chạy trước
+ *   php artisan passport:keys
+ */
+class DeployAuthSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * Deploy chỉ cần đăng nhập admin:
-     *   php artisan db:seed --class=DeployAuthSeeder
-     */
     public function run(): void
     {
         $this->call([
             UserSeeder::class,
-            CategoryPSeeder::class,
-            CategoryNSeeder::class,
             PermissionSeeder::class,
             RoleSeeder::class,
             RolePermissionSeeder::class,
